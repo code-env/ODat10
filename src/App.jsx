d75413098f2828476f854270705b88da0d83138f
Njingti Shanelle <alhadjioum0@gmail.com>
2024-07-15 19:13:06 +0100

    created about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Component from "./components/Component";
+import About from "./components/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/component" element={<Component />} />
+          <Route path="*" element={<About />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+    
+      <div className=" flex flex-col m-3 p-3 w-1/2">
+        <span>
+          About <h1>OD & OD@10</h1>
+        </span>
+        <p className="">
+          Open Dreams is a distinguished organization dedicated to providing
+          equal access to educational opportunities for underserved communities.
+          They achieve this by establishing educational centers, offering
+          scholarships, and providing mentorship programs. This year, Open
+          Dreams is poised to mark its 10th year of transforming lives and
+          shaping futures. The highly anticipated Open Dreams 10th Anniversary
+          Celebration is set to take place from July 25th to July 27th, 2024.
+          The festivities will kick off with an exhilarating hike on July 20th,
+          and end with a thanksgiving service on July 28th, 2024, marking a
+          decade of transformative impact and triumph. The upcoming anniversary
+          promises to be an unforgettable celebration of excellence, impact, and
+          collaboration, bringing together scholars, program facilitators,
+          mentors, volunteers, donors, educational stakeholders, and supporters
+          in a grand showcase of success and inspiration
+        </p>
+
+        <h2>Event Overview</h2>
+
+        <p>
+          The Open Dreams 10th Anniversary will span three days of immersive
+          experiences, thought-provoking discussions, engaging workshops, and
+          celebratory moments. The event will commence with a scenic hike up
+          Mount Nkol’so, setting the tone for the adventure and excitement that
+          will unfold over the following days at prestigious venues,
+          specifically at the IRIC Campus and Djeuga Palace Hotel.
+        </p>
+      </div>
+      <div>
+        <div className="bg-gray-200">
+            
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
