794d284412a28d1a0c15cac201162911cc62a293
shanelle-hub <njingtishanelle103@gmail.com>
2024-07-17 16:28:36 +0100

    partner page complete

---
+++ src/assets/Images/index.js
@@ -0,0 +1,14 @@
+import image2 from './image2.svg'
+import image5 from './image5.svg'
+import image4 from './image4.svg'
+import image6 from './image6.svg'
+import image7 from './image7.svg'
+import image8 from './image8.svg'
+import image9 from './image9.svg'
+import image10 from './image10.svg'
+import image11 from './image11.svg'
+import image12 from './image12.svg'
+import image13 from './image13.svg'
+import image3 from './image3.svg'
+
+export{image2,image10,image11,image12,image13,image4,image5,image6,image7,image8,image9,image3}
