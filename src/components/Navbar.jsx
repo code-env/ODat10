f30cabd9499d2be512e992c34bc8b11fefbd4ae0
shanelle-hub <njingtishanelle103@gmail.com>
2024-07-18 18:35:45 +0100

    added background image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
 
 
   return (
-    <nav className="fixed w-full top-0 right-0 left-0 z-50 bg-white">
+    <nav className="fixed w-full top-0 right-0 left-0 z-5">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 sm:py-5 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div>
