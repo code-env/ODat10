32a29e2b3b76bc6b382a7414f33268f8d0bd419f
shanelle-hub <njingtishanelle103@gmail.com>
2024-07-18 19:53:15 +0100

    added agenda

---
+++ src/components/Agenda.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Agenda() {
+ 
+  return (
+    <div>
+      <p>
+      </p>
+    </div>
+  );
+}
+
+export default Agenda;
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,10 +1,76 @@
 import React, { useState } from "react";
+import Agenda from "./Agenda";
 
 function Schedule() {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(0);
+  const myAgenda = [
+    {
+      name:"Opening Ceremony & Breakout Sessions - Venue: IRIC Amphitheatre",
+      title: "Day 1",
+      activities: [
+        "Registration & Welcome Coffee",
+        "Opening Ceremony",
+        "Keynote Speeches from educational authorities in Cameroon, local administrators, Open Dreams Co-founder",
+        "Insights and Discussions on International Education",
+        "Training sessions for counselors and high school teachers",
+        "Networking Lunch",
+        "Breakout Sessions",
+        "Dance Performances & Entertainment Acts",
+        "Counselors: Informative Panels",
+        "Group Reporting & Recommendations Sharing",
+        "Closing Remarks & Transition",
+      ],
+    },
+    {
+      name:" Workshops, Contests, & Guest Speakers - Venue: IRIC focused on students",
+      title: "Day 2",
+      activities: [
+        "Morning Coffee & Networking",
+        "Keynote Address on International Education",
+        "Concurrent Workshops (Various Topics)",
+        "High School Students: Opportunities & Access",
+        "University Students: Graduate-Level Summit",
+        "Networking Lunch",
+        "Guest Speaker Sessions",
+        "Essay Writing Contest",
+        "Public Speaking Competition",
+        "Networking Reception",
+      ],
+    },
+    {
+      name:"Festivities & Gala - Venue: Djeuga Palace",  
+      title: "Day 3",
+      activities: [
+        "Arrival of Members of the Open Dreams Family, Djeuga Palace Hotel ",
+        "Arrival of Guests",
+        "Arrival of VIPs ",
+        "Cameroon Anthem, OD Anthem",
+        "Keynote Speeches from Diplomatic Bodies, Ministers, Vice Chancellors, and Key Representatives",
+        "Panel Discussion with Visiting University Representatives, OD Team, and OD Scholars",
+        "Reflection on Open Dreams' 10-Year Journey",
+        "Cultural Parade of OD Scholars according to their country of study",
+        "Recognition and awards",
+        "Networking Lunch and Community Engagement Fair Kickoff",
+        "Community Engagement Fair - Interactive Booths, Exhibits, and Activities",
+        "Gala Ticket Sales & VIP Arrivals",
+        "Gala Program",
+        "Awards Ceremony: Recognizing Scholars, Mentors, Volunteers",
+        "Keynote Speeches & Fundraising Activities",
+        "Gourmet Dinner & Networking",
+        "Live Musical Performances & Entertainment",
+        "Event Concludes with a Celebratory Toast",
+      ],
+    },
+    {
+        name:"ThanksGiving service",
+        title:"Day 4",
+        activities:[
+            "As part of the celebration we will have a Thanksgiving Service to praise and honor God for the 10 years of triumph, impact, and community growth."
+        ]
+    }
+  ];
 
-  const items = Array.from({ length: 4 }, (_, i) => `DAY ${i + 1}`);
-
+  const items = Array.from({ length: 4 }, (_, i) => `Day ${i + 1}`);
   return (
     <div className="flex flex-col items-center">
       <div className="w-full md:w-auto">
@@ -24,6 +90,14 @@ function Schedule() {
             </button>
           ))}
         </div>
+         <div>
+          <h1>
+            {myAgenda[selected].title}
+          </h1>
+          <ul>{myAgenda[selected].activities.map((item)=>(
+            <li key={item}>{item}</li>
+          ))}</ul>
+         </div>
       </div>
     </div>
   );
